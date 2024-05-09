220d85fd0e8a6624e8e9e5b7377d6c5146e3ce51
wgallagher89 <wkgallagher@gmail.com>
2024-05-08 20:08:11 -0700

    Message generator complete

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 // Mixed Messages JavaScript Project
 
+// Object to hold 3 arrays to hold random output for messages
+const mixedMessages = {
+    favShows: ['Game of Thrones', 'Breaking Bad', 'Entourage', 'Lost', 'Succession', 'Ted Lasso'],
+    favActor: ['Peter Dinklage', 'Bryan Cranston', 'Adrian Grenier', 'Matthew Fox', 'Brian Cox', 'Phil Dunster'],
+    nextShows: ['Manhunt', 'The Regime', 'Shogun', 'Masters of the Air', 'Stranger Things', 'House of Dragons']
+};
+
+// Function to return randomized output from each array
+const randomizer = obj => {
+    const tempArr = [];
+    for (arr in obj) {
+        switch (arr) {
+            case 'favShows': 
+                tempArr.push(obj[arr][Math.floor(Math.random() * obj[arr].length)]);
+                break;
+            case 'favActor':
+                tempArr.push(obj[arr][Math.floor(Math.random() * obj[arr].length)]);
+                break;
+            case 'nextShows':
+                tempArr.push(obj[arr][Math.floor(Math.random() * obj[arr].length)]);
+                break;
+        }
+    }
+    return tempArr;
+};
+
+// Function to generate the mixed messages
+const mixedMessageGenerator = obj => {
+    const getMessages = randomizer(obj);
+    console.log(`Your favorite show is: ${getMessages[0]}`);
+    console.log(`Your favorite actor is: ${getMessages[1]}`);
+    console.log(`The next show you'll watch is: ${getMessages[2]}`);
+}
+
+mixedMessageGenerator(mixedMessages);
